e8244a812046fdfd07d1a68d5eeb7db7a32c7abc
Darwin <38464772+darwinknowless@users.noreply.github.com>
2021-08-06 19:34:20 +0700

    numbers

---
+++ numbers/numbers.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number1 int = 12
+	var number2 float32 = 3.151515
+	var number3 int = -15
+
+	fmt.Println(number1)
+	fmt.Println(number2)
+	fmt.Println(number3)
+
+}
